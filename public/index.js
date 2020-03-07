4bca56dfb5dd8cccfdc9fab70cdbd86335e386f1
Dario Turchi <darioturchi@outlook.com>
2020-03-07 15:27:01 -0500

    continue to update the dummy data and skeleton structure for GUI, Also added .js code to dynamically add vertical scroll if file log > 5

--- public/index.js
+++ public/index.js
@@ -1,43 +1,45 @@
 // Put all onload AJAX calls here, and event listeners
 $(document).ready(function() {
-    if ($('#log-table > tr').length > 5)
-    $('#log-table').css('overflow-y', 'scroll');
-    // Denis A3 stub
-    // On page-load AJAX Example
-    $.ajax({
-        type: 'get',            //Request type
-        dataType: 'json',       //Data type - we will use JSON for almost everything 
-        url: '/someendpoint',   //The server endpoint we are connecting to
-        data: {
-            name1: "Value 1",
-            name2: "Value 2"
-        },
-        success: function (data) {
-            /*  Do something with returned object
-                Note that what we get is an object, not a string, 
-                so we do not need to parse it on the server.
-                JavaScript really does handle JSONs seamlessly
-            */
-            $('#blah').html("On page load, received string '"+data.foo+"' from server");
-            //We write the object to the console to show that the request was successful
-            console.log(data); 
-
-        },
-        fail: function(error) {
-            // Non-200 return, do something with error
-            $('#blah').html("On page load, received error from server");
-            console.log(error); 
-        }
-    });
-
-    // Event listener form example , we can use this instead explicitly listening for events
-    // No redirects if possible
-    $('#someform').submit(function(e){
-        $('#blah').html("Form has data: "+$('#entryBox').val());
-        e.preventDefault();
-        //Pass data to the Ajax call, so it gets passed to the server
+    // enable vertical scrolling if the file log panel contains more than 5 files
+    if ($('#log-table tr').length > 6)
+    $('#log-table').addClass('enable-scroll');
+    
+    // -------------- Denis A3 stub --------------
+        // On page-load AJAX Example
         $.ajax({
-            //Create an object for connecting to another waypoint
+            type: 'get',            //Request type
+            dataType: 'json',       //Data type - we will use JSON for almost everything 
+            url: '/someendpoint',   //The server endpoint we are connecting to
+            data: {
+                name1: "Value 1",
+                name2: "Value 2"
+            },
+            success: function (data) {
+                /*  Do something with returned object
+                    Note that what we get is an object, not a string, 
+                    so we do not need to parse it on the server.
+                    JavaScript really does handle JSONs seamlessly
+                */
+                $('#blah').html("On page load, received string '"+data.foo+"' from server");
+                //We write the object to the console to show that the request was successful
+                console.log(data); 
+
+            },
+            fail: function(error) {
+                // Non-200 return, do something with error
+                $('#blah').html("On page load, received error from server");
+                console.log(error); 
+            }
         });
-    });
+
+        // Event listener form example , we can use this instead explicitly listening for events
+        // No redirects if possible
+        $('#someform').submit(function(e){
+            $('#blah').html("Form has data: "+$('#entryBox').val());
+            e.preventDefault();
+            //Pass data to the Ajax call, so it gets passed to the server
+            $.ajax({
+                //Create an object for connecting to another waypoint
+            });
+        });
 });
